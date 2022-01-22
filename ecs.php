7688d034239aae1c475ca3aaa25280e5915c7c7d
zingimmick <zingimmick@outlook.com>
2022-01-22 18:48:06 +0800

    Restore the package infrastructure

---
+++ ecs.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\ValueObject\Option;
+use Zing\CodingStandard\Set\ECSSetList;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(ECSSetList::PHP_72);
+    $containerConfigurator->import(ECSSetList::CUSTOM);
+
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::PARALLEL, true);
+    $parameters->set(
+        Option::SKIP,
+        [
+            \PHP_CodeSniffer\Standards\PSR1\Sniffs\Classes\ClassDeclarationSniff::class => ['*/migrations/*'],
+            // Will be removed in a future major version.
+            \SlevomatCodingStandard\Sniffs\TypeHints\ReturnTypeHintSniff::class,
+        ]
+    );
+    $parameters->set(
+        Option::PATHS,
+        [__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/ecs.php', __DIR__ . '/rector.php']
+    );
+};
