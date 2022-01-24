ce58b9d1b69507850c9578ac9a1ca1049d0097db
zingimmick <zingimmick@outlook.com>
2022-01-24 14:56:03 +0800

    Fix illegal offset type

--- rector.php
+++ rector.php
@@ -17,7 +17,7 @@ use Zing\CodingStandard\Set\RectorSetList;
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(RectorSetList::CUSTOM);
     $containerConfigurator->import(PHPUnitSetList::PHPUNIT_CODE_QUALITY);
-    $containerConfigurator->import(LevelSetList::UP_TO_PHP_72);
+    $containerConfigurator->import(LevelSetList::UP_TO_PHP_73);
 
     $parameters = $containerConfigurator->parameters();
     $parameters->set(
