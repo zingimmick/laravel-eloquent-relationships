b5e4726d486b19d8644022c7b0b37041fb477505
Zing <zingimmick@outlook.com>
2022-05-17 19:48:40 +0800

    Use ESCConfig instead of parameters (#18)

--- ecs.php
+++ ecs.php
@@ -3,12 +3,10 @@
 declare(strict_types=1);
 
 use Symplify\EasyCodingStandard\Config\ECSConfig;
-use Symplify\EasyCodingStandard\ValueObject\Option;
 use Zing\CodingStandard\Set\ECSSetList;
 
 return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->sets([ECSSetList::PHP_73, ECSSetList::CUSTOM]);
-    $parameters = $ecsConfig->parameters();
     $ecsConfig->parallel();
     $ecsConfig->skip([
         \PHP_CodeSniffer\Standards\PSR1\Sniffs\Classes\ClassDeclarationSniff::class => ['*/migrations/*'],
@@ -19,8 +17,5 @@ return static function (ECSConfig $ecsConfig): void {
         // Will be removed in a future major version.
         \SlevomatCodingStandard\Sniffs\TypeHints\ReturnTypeHintSniff::class,
     ]);
-    $parameters->set(
-        Option::PATHS,
-        [__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/ecs.php', __DIR__ . '/rector.php']
-    );
+    $ecsConfig->paths([__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/ecs.php', __DIR__ . '/rector.php']);
 };
