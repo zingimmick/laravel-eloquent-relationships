d4981e3746898cea09b51a75a040d81a6adc329d
Zing <zingimmick@outlook.com>
2024-11-06 02:38:36 +0800

    Fix PHPStan issues (#77)

--- src/Relations/MorphToOne.php
+++ src/Relations/MorphToOne.php
@@ -16,8 +16,9 @@ use Illuminate\Database\Query\JoinClause;
 
 /**
  * @template TRelatedModel of \Illuminate\Database\Eloquent\Model
+ * @template TDeclaringModel of \Illuminate\Database\Eloquent\Model
  *
- * @extends \Illuminate\Database\Eloquent\Relations\MorphToMany<TRelatedModel>
+ * @extends \Illuminate\Database\Eloquent\Relations\MorphToMany<TRelatedModel, TDeclaringModel>
  */
 class MorphToOne extends MorphToMany implements SupportsPartialRelations
 {
